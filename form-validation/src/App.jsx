9b0a76039c1f7ac0fab30d76798a4bf18b2e9ac1
Sthitaprajna Sahu <sthitasahu074@gmail.com>
2024-10-18 23:33:03 +0530

    complete dthe form validation project

--- form-validation/src/App.jsx
+++ form-validation/src/App.jsx
@@ -1,34 +1,111 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [username,setusername]=useState("")
+  const [password,setpassword]=useState("")
+  const [confirmpassword,setconfirmpassword]=useState("")
+  const [email,setemail]=useState("")
+
+
+  const [errorUserName, setErrorUserName] = useState("");
+  const [errorEmail, setErrorEmail] = useState("");
+  const [errorPassword, setErrorPassword] = useState("");
+  const [errorConfirmPassword, setErrorConfirmPassword] = useState("");
+
+  const [usernameColor,setusernameColor]=useState("")
+  const [passwordColor,setpaswordColor]=useState("")
+  const [confirmpasswordColor,setconfirmpasworColor]=useState("")
+  const [emailColor,setemailColor]=useState("")
+
+  function check(e){
+     e.preventDefault()
+     if(username.length <8){
+      setErrorUserName("Username should be atleast 8 letters long")
+      setusernameColor("red")
+     }else{
+       setErrorUserName("")
+       setusernameColor("green")
+     }
+     if(password.length >=8){
+        setErrorPassword("")
+        setpaswordColor("green")
+     }
+     else{
+       setErrorPassword("Password must be atleast 8 characters long")
+       setpaswordColor("red")
+     }
+     if(email.includes("@gmail.com")){
+      setErrorEmail("")
+      setemailColor("green")
+     }
+     else{
+       setErrorEmail("Invalid email");
+       setemailColor("red")
+     }
+
+     if(password != "" && password ===confirmpassword){
+       setErrorConfirmPassword("")
+       setconfirmpasworColor("green")
+     }
+     else{
+       setErrorConfirmPassword("Passwords donot match")
+       setconfirmpasworColor("red")
+     }
+  }
 
   return (
+    
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="card">
+      <div className="card-image"></div>
+
+      <form>
+        <input
+          type="text"
+          placeholder="Name"
+          style={{ borderColor:usernameColor }}
+          value={username}
+          onChange={(e) => setusername(e.target.value)}
+        />
+
+        <p className="error">{errorUserName}</p>
+
+        <input
+          type="text"
+          placeholder="Email"
+          style={{ borderColor: emailColor }}
+          value={email}
+          onChange={(e) => setemail(e.target.value)}
+        />
+        <p className="error">{errorEmail}</p>
+
+        <input
+          type="password"
+          placeholder="Password"
+          style={{ borderColor: passwordColor }}
+          value={password}
+          onChange={(e) =>setpassword(e.target.value)}
+        />
+        <p className="error">{errorPassword}</p>
+
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          style={{ borderColor: confirmpasswordColor }}
+          value={confirmpassword}
+          onChange={(e) =>setconfirmpassword(e.target.value) }
+        />
+        <p className="error">{errorConfirmPassword}</p>
+
+        <button className="submit-btn" onClick={check}>
+          Submit
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </form>
+    </div>
+  </>
+    
   )
 }
 
